be1aa196fc8191de911f8b555ff57db15f424aea
Kuba <kuba.pocztowski99@gmail.com>
2025-06-29 16:40:02 +0200

    changed playfromboot script

--- Assets/Editor/PlayFromBoot.cs
+++ Assets/Editor/PlayFromBoot.cs
@@ -1,34 +1,13 @@
 using UnityEditor;
 using UnityEditor.SceneManagement;
-using UnityEngine.SceneManagement;
 
 [InitializeOnLoad]
 public static class PlayFromBoot
 {
-    static string bootScenePath = "Assets/Scenes/Boot.unity";
-    static string previousScenePath;
-
     static PlayFromBoot()
     {
-        EditorApplication.playModeStateChanged += OnPlayModeChanged;
-    }
-
-    private static void OnPlayModeChanged(PlayModeStateChange state)
-    {
-        if (state == PlayModeStateChange.ExitingEditMode)
-        {
-            previousScenePath = SceneManager.GetActiveScene().path;
-
-            if (!EditorSceneManager.GetSceneByPath(bootScenePath).isLoaded)
-            {
-                EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
-                EditorSceneManager.OpenScene(bootScenePath);
-            }
-        }
-
-        if (state == PlayModeStateChange.EnteredEditMode && !string.IsNullOrEmpty(previousScenePath))
-        {
-            EditorSceneManager.OpenScene(previousScenePath);
-        }
+        var boot = AssetDatabase.LoadAssetAtPath<SceneAsset>("Assets/Scenes/Boot.unity");
+        if (boot != null)
+            EditorSceneManager.playModeStartScene = boot;
     }
 }
