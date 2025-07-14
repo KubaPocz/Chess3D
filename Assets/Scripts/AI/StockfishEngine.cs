078573445b76a9443d47bfab8b4422206042d4e8
Kuba <kuba.pocztowski99@gmail.com>
2025-07-14 17:20:26 +0200

    uploading stocktish and implementation of stockfish engine script

---
+++ Assets/Scripts/AI/StockfishEngine.cs
@@ -0,0 +1,63 @@
+using System.Diagnostics;
+using UnityEngine;
+
+public class StockfishEngine
+{
+    private Process process;
+
+    public void StartEngine()
+    {
+        process = new Process();
+        process.StartInfo.FileName = Application.streamingAssetsPath + "/AI/strockfish.exe";
+        process.StartInfo.RedirectStandardInput = true;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.CreateNoWindow = true;
+        process.Start();
+
+        SendCommand("uci");
+        SendCommand("uciok");
+        SendCommand("isready");
+        SendCommand("reakyok");
+    }
+    public void StopEngine()
+    {
+        SendCommand("quit");
+        process.Close();
+    }
+    public void SetSkillLevel(int level)
+    {
+        level = Mathf.Clamp(level, 0, 20);
+        SendCommand($"setoption name Skill Level value {level}");
+    }
+    public string GetBestMove(string fen, int depth = 10)
+    {
+        SendCommand("position fen " + fen);
+        SendCommand($"go depth {depth}");
+        
+        string line;
+        while((line = process.StandardOutput.ReadLine()) != null)
+        {
+            if (line.StartsWith("bestmove"))
+            {
+                return line.Split(' ')[1];
+            }
+        }
+        return null;
+    }
+    private void SendCommand(string command)
+    {
+        process.StandardInput.WriteLine(command);
+        process.StandardInput.Flush();
+    }
+
+    private void WaitFor(string keyword)
+    {
+        string line;
+        while((line=process.StandardOutput.ReadLine()) != null)
+        {
+            if (line.Contains(keyword))
+                return;
+        }
+    }
+}
