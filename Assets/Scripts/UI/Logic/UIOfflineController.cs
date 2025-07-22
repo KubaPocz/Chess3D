e31cbe97d3f99a54ae970fd20bde4b01d58f6f38
Kuba <kuba.pocztowski99@gmail.com>
2025-07-22 18:05:21 +0200

    created ui in UIOfflineScene and base implementation of updating data in it

--- Assets/Scripts/UI/Logic/UIOfflineController.cs
+++ Assets/Scripts/UI/Logic/UIOfflineController.cs
@@ -1,16 +1,58 @@
+using TMPro;
+using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEngine;
 
 public class UIOfflineController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private TextMeshProUGUI TurnLabel;
+    [SerializeField] private TextMeshProUGUI MovesLabel;
+    [SerializeField] private TextMeshProUGUI CapturedLabel;
+    [SerializeField] private TextMeshProUGUI DifficultyLabel;
+    [SerializeField] private TextMeshProUGUI TimerLabel;
     void Start()
     {
-        
-    }
+        GameEvents.OnChangeTurnRequested += UpdateTurnLabel;
+        GameEvents.OnAddPlayerMoveRequested += UpdateMovesCountLabel;
 
-    // Update is called once per frame
-    void Update()
+        BootLabels();
+    }
+    private void BootLabels()
     {
-        
+        if (GameConfigStore.CurrentConfig.PlayerColor == GameStats.Instance.currentTurnColor)
+            TurnLabel.text = "Player Turn";
+        else
+            TurnLabel.text = "Bot Turn";
+        MovesLabel.text = 0.ToString();
+        CapturedLabel.text = 0.ToString();
+        string difficulty;
+        int value = GameConfigStore.CurrentConfig.Difficulty;
+        if (value <= 2) difficulty = "Very Easy";
+        else if (value <= 4) difficulty = "Easy";
+        else if (value <= 7) difficulty = "Medium";
+        else if (value <= 10) difficulty = "Challenging";
+        else if (value <= 13) difficulty = "Hard";
+        else if (value <= 16) difficulty = "Very Hard";
+        else if (value <= 18) difficulty = "Expert";
+        else difficulty = "Master";
+        DifficultyLabel.text = difficulty;
+        TimerLabel.text = "15:00";
+    }
+    private void UpdateTurnLabel()
+    {
+        if (GameConfigStore.CurrentConfig.PlayerColor == GameStats.Instance.currentTurnColor)
+            TurnLabel.text = "Player Turn";
+        else
+            TurnLabel.text = "Bot Turn";
+    }
+    private void UpdateMovesCountLabel()
+    {
+        if (GameConfigStore.CurrentConfig.PlayerColor == ChessColor.White)
+            MovesLabel.text = GameStats.Instance.whiteMoves.ToString();
+        else 
+            MovesLabel.text = GameStats.Instance.blackMoves.ToString();
+    }
+    private void UpdateTimerLabel()
+    {
+
     }
 }
